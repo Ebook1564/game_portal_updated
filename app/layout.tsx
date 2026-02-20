b44653103edf2badfe0a2991905b5b9e8b783ee8
Ebook1564 <myebookbundle@gmail.com>
2026-02-20 15:37:43 +0530

    add adsense creds

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import localFont from "next/font/local";
 import "./globals.css";
 import type { Metadata } from 'next';
+import Script from "next/script";
 
 const nekst = localFont({
   src: [
@@ -27,6 +28,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={nekst.variable}>
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6157121429363510"
+        crossOrigin="anonymous"
+        strategy="beforeInteractive"
+      />
       <body className="antialiased">
         {children}
       </body>
